05457b4cc179b9300411f3ec8195f5d64d75fb52
anjali7786 <guptaanj7786@gmail.com>
2022-01-19 12:06:21 +0530

    code added

---
+++ Adobe/Q1.cpp
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        // Your code here
+        int st=1,i=0,sum=0,j=0;
+        for(i=0;i<n;)
+        {
+            while(sum<s)
+            {
+                sum+=arr[i];
+                i++;
+            }
+            while(sum>s)
+            {
+                sum-=arr[st-1];
+                st++;
+            }
+            if(sum==s)
+            {
+                break;
+            }
+        }
+        if(sum==s)  return {st,i};
+        return {-1};
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
